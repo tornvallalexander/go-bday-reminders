1c5aea4bf58690059ad5dc107df7459e9720fbe3
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-03-05 12:25:40 +0100

    refactored twilio send message

--- main.go
+++ main.go
@@ -2,29 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/twilio/twilio-go"
-	openapi "github.com/twilio/twilio-go/rest/api/v2010"
-	"go-bday-reminders/utils"
-	"log"
+	"go-bday-reminders/sms"
 )
 
 func main() {
-	config, err := utils.LoadConfig(".")
-	if err != nil {
-		log.Fatal("Cannot load config", err)
-	}
-
-	client := twilio.NewRestClientWithParams(twilio.RestClientParams{
-		Username: config.AccountSid,
-		Password: config.AuthToken,
-	})
-
-	params := &openapi.CreateMessageParams{}
-	params.SetTo(config.Receiver)
-	params.SetFrom(config.Sender)
-	params.SetBody("Hello from Go!")
-
-	resp, err := client.ApiV2010.CreateMessage(params)
+	resp, err := sms.SendSms("Alexander", "2023-03-02")
 	if err != nil {
 		fmt.Println(err.Error())
 		err = nil
---
+++ sms/sendSms.go
@@ -0,0 +1,29 @@
+package sms
+
+import (
+	"fmt"
+	"github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
+	"go-bday-reminders/utils"
+)
+
+// SendSms send a message to the receiver with name and birthday
+func SendSms(name string, birthday string) (resp *openapi.ApiV2010Message, err error) {
+	config, err := utils.LoadConfig(".")
+	if err != nil {
+		return
+	}
+
+	client := twilio.NewRestClientWithParams(twilio.RestClientParams{
+		Username: config.AccountSid,
+		Password: config.AuthToken,
+	})
+
+	params := &openapi.CreateMessageParams{}
+	params.SetTo(config.Receiver)
+	params.SetFrom(config.Sender)
+	params.SetBody(fmt.Sprintf("Födelsedag alert: %s, %s", name, birthday))
+
+	resp, err = client.ApiV2010.CreateMessage(params)
+	return
+}
